a85a640c5a1ecffb651f18908ee0c45d76b352fc
kitagami <kitagami@artifactnoise.com>
2016-03-21 06:20:54 +0000

    ADT7410動作確認済み

--- examples/raspberrypi/ADT7410.py
+++ examples/raspberrypi/ADT7410.py
@@ -3,5 +3,7 @@ from akilib.raspberrypi import AKI_I2C_ADT7410
 import time
 
 adt7410 = AKI_I2C_ADT7410.AKI_I2C_ADT7410()
-adt7410.Init()
-print adt7410.read()
+while 1:
+	adt7410.Init()
+	print "%6.3F 'C" % adt7410.read() 	
+	time.sleep(1)
