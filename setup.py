d3c4c2ed8e179b1ebdc9cf1a832ecdf9b36ddb60
kitagami <kitagami@artifactnoise.com>
2016-04-23 16:43:29 +0900

    BugFix

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# python setup.py register sdist upload
+from setuptools import setup
+
+import os
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+long_description = f.read()
+f.close()
+
+setup(
+    name = 'akilib',
+    version = '2.0.9',
+    url="https://github.com/nonNoise/akilib",
+    keywords = ('Edison','Raspberry','IoT','akilib'),
+    description = 'This Library is Hardware Library. and You can buy parts in Japan Akihabara .',
+    license = 'MIT License',
+    install_requires = [],
+    long_description=long_description,
+    packages=['akilib','akilib/edison','akilib/raspberrypi'],
+    author = ' Yuta KItagami',
+    author_email = 'kitagami@artifactnoise.com',
+    platforms = ['Linux'],
+)
+
+
+
+ 
