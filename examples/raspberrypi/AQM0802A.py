d4b92503a9779a9b43b1c09b7a60f4df87885e53
nonnoise <hokusin02@gmail.com>
2016-10-18 11:18:31 +0900

    upgrade

--- examples/raspberrypi/AQM0802A.py
+++ examples/raspberrypi/AQM0802A.py
@@ -1,7 +1,10 @@
 from akilib.raspberrypi import AKI_I2C_AQM0802A
 import time
+import RPi.GPIO as GPIO
 
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.OUT)
 
 lcd = AKI_I2C_AQM0802A.AKI_I2C_AQM0802A()
 lcd.Init_LCD()
@@ -10,15 +13,19 @@ lcd.WritePos(0,0)
 
 print "LCD Testing ....."
 for i in range(0,10):
+	GPIO.setup(4,GPIO.OUT)
+	time.sleep(0.01)
 	lcd.ClearDisplay()
 	lcd.WritePos(0,0)
 	lcd.WriteStr("Hello")
-
+	GPIO.output(4,1)
 	lcd.WritePos(1,0)
 	lcd.WriteStr(str(i))
-	time.sleep(0.7)
+	time.sleep(0.5)
 	print i
-
+	GPIO.output(4,0)
+	time.sleep(0.5)
 lcd.ClearDisplay()
 lcd.WritePos(0,0)
 lcd.WriteStr("OK!!")
+GPIO.output(4,0)
